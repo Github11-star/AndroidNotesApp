34b049e471532951cf14770b6cca6dbdfcf37664
Firoz Shaikh <gitfiroz695@gmail.com>
2023-07-05 10:48:24 +0530

    View Binding

--- app/src/main/java/com/firozpocyt/androidnotesapp/RegisterFragment.kt
+++ app/src/main/java/com/firozpocyt/androidnotesapp/RegisterFragment.kt
@@ -7,22 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import com.firozpocyt.androidnotesapp.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+
+    private var _binding : FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_register, container, false)
-        val txtRedirect = view.findViewById<TextView>(R.id.txtRedirect)
-
-        txtRedirect.setOnClickListener {
+        _binding = FragmentRegisterBinding.inflate(inflater,container,false)
+        /*binding.txtRedirect.setOnClickListener {
             findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
-        }
+        }*/
 
-        return view
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
