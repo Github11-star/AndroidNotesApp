8e3703b86466c0e9ed27ca41e1ff432137d8fd52
Firoz Shaikh <gitfiroz695@gmail.com>
2023-07-25 10:26:57 +0530

    Android RecyclerView + ListAdpater

--- app/src/main/java/com/firozpocyt/androidnotesapp/MainFragment.kt
+++ app/src/main/java/com/firozpocyt/androidnotesapp/MainFragment.kt
@@ -1,38 +1,66 @@
 package com.firozpocyt.androidnotesapp
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.firozpocyt.androidnotesapp.api.NotesAPI
-import com.firozpocyt.androidnotesapp.utils.Constants.TAG
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.firozpocyt.androidnotesapp.databinding.FragmentMainBinding
+import com.firozpocyt.androidnotesapp.utils.NetworkResult
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
 
-    @Inject
-    lateinit var notesAPI: NotesAPI
+    private var _binding : FragmentMainBinding? = null
+    private val binding get() = _binding!!
+    private val noteViewModel by viewModels<NoteViewModel>()
+
+    private lateinit var adapter: NoteAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        CoroutineScope(Dispatchers.IO).launch {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        adapter = NoteAdapter()
+        return binding.root
+    }
 
-            val response = notesAPI.getNotes()
-            Log.d(TAG, response.body().toString())
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindObservers()
+        noteViewModel.getNote()
+        binding.noteList.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        binding.noteList.adapter = adapter
+    }
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    private fun bindObservers() {
+        noteViewModel.noteLiveData.observe(viewLifecycleOwner, Observer {
+                binding.progressBar.isVisible = false
+            when(it){
+                is NetworkResult.Success -> {
+                    adapter.submitList(it.data)
+                }
+                is NetworkResult.Error -> {
+                    Toast.makeText(requireContext(),it.message.toString(), Toast.LENGTH_LONG).show()
+                }
+                is NetworkResult.Loading -> {
+                    binding.progressBar.isVisible = true
+                }
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
---
+++ app/src/main/java/com/firozpocyt/androidnotesapp/NoteViewModel.kt
@@ -0,0 +1,40 @@
+package com.firozpocyt.androidnotesapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.firozpocyt.androidnotesapp.models.NoteRequest
+import com.firozpocyt.androidnotesapp.repository.NoteRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NoteViewModel @Inject constructor(private val noteRepository: NoteRepository): ViewModel() {
+
+    val noteLiveData get() = noteRepository.noteLiveData
+    val statusLiveData get() = noteRepository.statusLiveData
+
+    fun getNote(){
+        viewModelScope.launch {
+        noteRepository.getNotes()
+        }
+    }
+
+    fun createNote(noteRequest: NoteRequest){
+        viewModelScope.launch {
+            noteRepository.createNote(noteRequest)
+        }
+    }
+
+    fun updateNote(notedId : String, noteRequest: NoteRequest){
+        viewModelScope.launch {
+            noteRepository.updateNote(notedId,noteRequest)
+        }
+    }
+
+    fun deleteNote(notedId: String){
+        viewModelScope.launch {
+            noteRepository.deleteNote(notedId)
+        }
+    }
+}
